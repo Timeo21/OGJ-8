d118a4318346550ec55d02cb4878a705af788884
Yrixou <loris.vocetorquemada@epfl.ch>
2026-03-14 19:44:30 +0100

    fish are fishing

--- scripts/fish.gd
+++ scripts/fish.gd
@@ -1,28 +1,42 @@
 extends Node2D
 
-@export var speed = 10
-var point = Vector2.ZERO
+@export var speed_angular = 1.0
+@export var speed = 2.0
 var dir = Vector2.RIGHT
-var found_point = 0
+var time_elapsed =0
+@onready var animation : AnimatedSprite2D = $Area2D/AnimatedSprite2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	dir = dir.rotated(randf_range(-180,180)/(180*PI) * speed_angular)
+	animation.play();
 	pass # Replace with function body.
-
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if (!point or !dir or found_point == 1):
+	handle_sprite()
 		#print("generated a new point.")
 		#dir = Vector2.from_angle(randi_range(0,360))
-		point = Vector2(((-1)^randi_range(0,1))*randi()%340,((-1)^randi_range(0,1))*randi()%180)
-		found_point =0;
 		#print(point)
-	var t = point - position
-	if abs(dir.angle_to(t)) > 0.5:
-		dir = dir.rotated(randf_range(-360,360)/(180*PI))
-		position += dir.normalized() 
-	else:
-		print("Found point")
-		found_point =1
-		
+	#dwprint(speed)
+	time_elapsed += delta
+	if (time_elapsed > 0.5):
+		dir = dir.rotated(randf_range(-360,360)/(180*PI) * speed_angular * delta)
+		time_elapsed =0
+	position += dir.normalized() *speed *delta
+	if (position.x < -320):
+		dir = (dir.normalized() + Vector2.RIGHT).normalized() 
+	elif (position.x>320):
+		dir = (dir.normalized() + Vector2.LEFT).normalized()
+	elif (position.y >180):
+		dir = (dir.normalized() + Vector2.UP).normalized()
+	elif (position.y < -180):
+		dir = (dir.normalized() + Vector2.DOWN).normalized()
 	pass
+func handle_sprite() -> void:
+	if (dir.x < 0.2):
+		animation.flip_h = true
+	elif (dir.x > 0.2):
+		animation.flip_h = false
+		
+	
