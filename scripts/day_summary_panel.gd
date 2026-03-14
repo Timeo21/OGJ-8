b99edbfe768da5d01c39346e6d07dc55b8a0db86
Lalfada <67563085+Lalfada@users.noreply.github.com>
2026-03-14 17:18:31 +0100

    add day summary panel

---
+++ scripts/day_summary_panel.gd
@@ -0,0 +1,28 @@
+extends PanelContainer
+
+@export var easy_num: Label
+@export var normal_num: Label
+@export var hard_num: Label
+@export var easy_money: Label
+@export var normal_money: Label
+@export var hard_money: Label
+@export var total_money: Label
+
+func display_day_summary(day_summary: DaySummary) -> void:
+	var n1 := day_summary.easy_fish_number
+	var n2 := day_summary.normal_fish_number
+	var n3 := day_summary.hard_fish_number
+	easy_num.text = "Easy fish number: %d" % n1
+	normal_num.text = "Normal fish number: %d" % n2
+	hard_num.text = "Normal fish number: %d" % n3
+	
+	
+	var v1 := n1*Utils.EASY_FISH_WORTH
+	var v2 := n2*Utils.NORMAL_FISH_WORTH
+	var v3 := n3*Utils.HARD_FISH_WORTH
+	var tot := v1+v2+v3
+	easy_money.text = "→%d🪙" % v1
+	normal_money.text = "→%d🪙" % v2
+	hard_money.text = "→%d🪙" % v3
+	
+	total_money.text = "%d🪙" % tot
--- scripts/utils.gd
+++ scripts/utils.gd
@@ -1,3 +1,8 @@
 extends Node
 
 enum ItemId {SLOW, PREDICTION, FREEZE, TP, FAST}
+enum FishType {EASY, NORMAL, HARD}
+
+const EASY_FISH_WORTH: int = 1
+const NORMAL_FISH_WORTH: int = 2
+const HARD_FISH_WORTH: int = 5
