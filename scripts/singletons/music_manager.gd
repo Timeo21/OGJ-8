c5f289bc43878da332f62f46efbae42b260e9a0a
Yrixou <loris.vocetorquemada@epfl.ch>
2026-03-15 13:47:40 +0100

    music

--- scripts/singletons/music_manager.gd
+++ scripts/singletons/music_manager.gd
@@ -7,6 +7,7 @@ func _on_audio_stream_player_finished() -> void:
 	audio_player.play(21.883)
 
 func _ready() -> void:
+	audio_player.play()
 	SignalBus.button_clicked.connect(play_click)
 	
 func play_click() -> void:
