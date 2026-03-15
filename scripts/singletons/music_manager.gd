d5c5f95e929ac047257e071a9109fcca292632b3
Yrixou <loris.vocetorquemada@epfl.ch>
2026-03-15 13:52:56 +0100

    dzwezf

--- scripts/singletons/music_manager.gd
+++ scripts/singletons/music_manager.gd
@@ -2,16 +2,12 @@ extends Node
 
 @export var audio_player: AudioStreamPlayer
 @export var click_player: AudioStreamPlayer
-var played = false
+
 func _on_audio_stream_player_finished() -> void:
 	audio_player.play(21.883)
 
-func _input(event: InputEvent) -> void:
-	if not played:
-		audio_player.play()
-		played = true
-
 func _ready() -> void:
+	audio_player.play()
 	SignalBus.button_clicked.connect(play_click)
 	
 func play_click() -> void:
